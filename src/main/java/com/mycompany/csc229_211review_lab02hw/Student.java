e9710ff659da8691ee58b2e51379a14e1b5db3e9
ranveersingh <ranveersingh@ranveers-air.lan>
2023-02-04 03:08:04 +0000

    Make Student class a child of Person

---
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -0,0 +1,28 @@
+package com.mycompany.csc229_211review_lab02hw;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class Student {
+	// ToDo 1: Make this class a child of Person
+	
+	// ToDo 2: Fix the resulting errors
+	
+	// ToDo 3: Add a field for GPA and create setter and getter
+	
+	// ToDo 4: Add comments to your code
+
+}
+
+class Student extends Person {
+    private String name;
+    private String address;
+    private short age;
+    
+  private double GPA;
+  
+  // GPA instance variable 
+    
+}
+
