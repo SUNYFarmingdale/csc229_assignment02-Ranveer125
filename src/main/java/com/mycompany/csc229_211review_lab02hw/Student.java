d7b026bf70263d26fe44b4f4e4103184df653464
ranveersingh <ranveersingh@ranveers-air.lan>
2023-02-07 18:55:36 -0500

    Make Student class a child of Person

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -1,20 +1,16 @@
 package com.mycompany.csc229_211review_lab02hw;
+
 //
 ///**
 /*
  * @author MoaathAlrajab
  */
-public class Student {
-
-    Student(String james, int i) {
-        
+public abstract class Student extends Person{
+     
+        public Student(String name, short age) {
+        super(name, age);
     }
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
-
+  
+   // Constructor for Data Members from DriverClass 
+          
 }
