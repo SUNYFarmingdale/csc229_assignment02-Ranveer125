d4053616e3b5332e0412723a45395b6ef186c03a
ranveersingh <ranveersingh@ranveers-air.lan>
2023-02-08 18:43:07 -0500

    Make Student class a child of Person

--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -5,12 +5,63 @@ package com.mycompany.csc229_211review_lab02hw;
 /*
  * @author MoaathAlrajab
  */
-public abstract class Student extends Person{
-     
-        public Student(String name, short age) {
-        super(name, age);
+public class Student extends Person {
+    
+         private double GPA;         
+
+    public double getGPA() {
+        return GPA;
     }
-  
-   // Constructor for Data Members from DriverClass 
-          
+/**
+ * 
+ * @param GPA gets the value /getter
+ */
+    public void setGPA(double GPA) {
+        this.GPA = GPA;
+    }
+    /**
+     * sets the value /setter
+     */
+    
+         private String address;       
+         
+     // Instance variables for GPA and address
+
+         public Student(String name, short age) {
+             super(name, age);
+             address="";
+    }
+         
+        public Student(String name, short age, double GPA, String address) {
+        super(name, age);
+        
+        this.GPA = GPA;
+        this.address = address;
+    }
+        
+    @Override
+
+    public String getAddress() {
+        return address;
+ 
+    }
+    // Override Method for Implementation on Address
+
+    @Override
+
+    public void setAddress(String address) {
+        this.address=address;
+
+    }
+
+   
+	// ToDo 1: Make this class a child of Person
+	
+	// ToDo 2: Fix the resulting errors
+	
+	// ToDo 3: Add a field for GPA and create setter and getter
+	
+	// ToDo 4: Add comments to your code
+
 }
+
