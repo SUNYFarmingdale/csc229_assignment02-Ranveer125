d71f909356e70df998c3b2314e96b93dcec7dfab
ranveersingh <ranveersingh@ranveers-air.lan>
2023-02-09 15:55:21 -0500

    Set gpa of student using scanner and user method to print output

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,5 +1,7 @@
 package com.mycompany.csc229_211review_lab02hw;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MoaathAlrajab
@@ -14,21 +16,36 @@ public class DriverClass {
 		
 		// ToDo 7: Add a toString method for Student class
 		
-		Student std1= new Student("James", (short) 20);
+        Student std1= new Student("James", (short) 20);
           
-        // Fix error by setting 20 to short
+     // Fix error by setting 20 to short
         
-		
+         Scanner scanner = new Scanner(System.in);
+           
+         System.out.print("Enter "+std1.getName()+" GPA score: ");
+            
+         double gpa = scanner.nextDouble();
+             
+        // Add scanner and user method and print output
+
+         std1.setGPA(gpa);
+              
+        // Set GPA for std1
+              
 		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
+		// input and then print the output.
 		
-		System.out.println(std1);
-		
+                   System.out.println(std1);
+        
+	// Implemented toString method inside Student Class
+        
+        
 		// ToDo 9: add comments and explain your code
 		
 		// ToDo 10: submit using a pull request.
 	}
 }
+
 
 
 
